27479e73fe9b658ad9ed31904c25e471c726e0ef
Your Name <you@example.com>
2022-05-13 08:07:53 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -16,7 +16,7 @@ void show(Node *n) {
             else putchar (' ');
 
         }
-        if ((i+2) % 2 == 0) {
+        if ((i) % 2 == 0) {
             putchar('\n');
             for(int i = 0; i < 18; i++) putchar ('-');
         }
